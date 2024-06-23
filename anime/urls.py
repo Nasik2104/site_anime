8a1c97859683f107c73060c0fdd1e072ad10215c
unknown <gavrisnazar981@gmail.com>
2024-06-23 12:51:54 +0300

    add_episode

--- anime/urls.py
+++ anime/urls.py
@@ -6,5 +6,6 @@ app_name = 'anime'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('view/<int:id>', views.view_anime, name='view')
+    path('view/<int:anime_id>', views.view_anime, name='view'),
+    path('add_episode/', views.add_episode, name='add_episode')
 ]
--- anime/views.py
+++ anime/views.py
@@ -1,15 +1,42 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, HttpResponse, redirect
 
-from .models import Anime, Episode
+from .models import Anime, Episode, VoiceActingTeam
 
 
 def index(request):
     anime = Anime.objects.all()
-    return render(request, 'index.html', {'anime': anime})
+    return render(request, 'index.html', {'anime': anime, 'title': 'Anima'})
 
 
-def view_anime(request, id):
-    anime = get_object_or_404(Anime, id=id)
+def view_anime(request, anime_id):
+    anime = get_object_or_404(Anime, id=anime_id)
     episodes = Episode.objects.filter(anime=anime)
-    return render(request, 'view.html', {'anime': anime})
+    return render(request, 'view.html', {'anime': anime, 'title': anime.name})
 
+
+def add_episode(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        video = request.FILES.get('video')
+        anime_id = request.POST.get('anime')
+        team_id = request.POST.get('voice_acting_team')
+
+        try:
+            anime = Anime.objects.get(id=anime_id)
+            team = VoiceActingTeam.objects.get(id=team_id)
+            new_episode = Episode(name=name, video=video, anime=anime, voice_acting_team=team)
+            new_episode.full_clean()
+            new_episode.save()
+        except Anime.DoesNotExist:
+            return HttpResponse('Invalid anime selected.')
+        except VoiceActingTeam.DoesNotExist:
+            return HttpResponse('Invalid voice acting team selected.')
+        except Exception as e:
+            return HttpResponse(f'Error: {str(e)}')
+        else:
+            return redirect('anime:index')
+
+    animes = Anime.objects.all()
+    teams = VoiceActingTeam.objects.all()
+
+    return render(request, 'add_episode.html', {'animes': animes, 'teams': teams})
